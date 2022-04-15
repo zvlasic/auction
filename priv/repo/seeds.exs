95b76fcb989a84cdffb12265d1e6c36ec22730b3
zvlasic <zlayo1@gmail.com>
2022-04-15 15:24:48 +0200

    Define basic seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,21 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Auction.Repo.insert!(%Auction.Schemas.Item{
+  title: "WarGames Bluray",
+  description: "The best computer movie of all time, now on Bluray!",
+  ends_at: DateTime.utc_now()
+})
+
+Auction.Repo.insert!(%Auction.Schemas.Item{
+  title: "U2 - Achtung Baby on CD",
+  description: "The sound of 4 men chopping down The Joshua Tree",
+  ends_at: DateTime.utc_now()
+})
+
+Auction.Repo.insert!(%Auction.Schemas.Item{
+  title: "My first item",
+  description: "A tasty item sure to please",
+  ends_at: DateTime.utc_now()
+})
