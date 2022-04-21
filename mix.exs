cceb15517d4a04c031598311dbb988ec0ef3ad38
zvlasic <zlayo1@gmail.com>
2022-04-21 11:15:15 +0200

    Introduce credo

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Auction.MixProject do
       {:telemetry_metrics, "~> 0.6"},
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.6"}
     ]
   end
 
