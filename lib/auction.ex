cf3dd840e6a2dd3177f3623cc83e3c678ec30148
zvlasic <zlayo1@gmail.com>
2022-04-21 10:56:48 +0200

    Add create item action

--- lib/auction.ex
+++ lib/auction.ex
@@ -5,4 +5,5 @@ defmodule Auction do
   def items, do: Repo.all(Item)
   def get_item(id), do: Repo.get(Item, id)
   def new_item, do: Item.changeset(%Item{})
+  def insert_item(attrs), do: Repo.insert(Item.changeset(%Item{}, attrs))
 end
