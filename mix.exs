966f6080a91492db5f607120ba8764e32f259c01
zvlasic <zlayo1@gmail.com>
2022-04-25 14:00:46 +0200

    Add pbkdf2

--- mix.exs
+++ mix.exs
@@ -46,7 +46,8 @@ defmodule Auction.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:pbkdf2_elixir, "~> 2.0"}
     ]
   end
 
