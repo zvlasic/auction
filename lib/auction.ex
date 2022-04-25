e0df7fcd5a329f46152d152406f8df394d63ebfe
zvlasic <zlayo1@gmail.com>
2022-04-25 14:02:20 +0200

    Adapt styling

--- lib/auction.ex
+++ lib/auction.ex
@@ -7,9 +7,9 @@ defmodule Auction do
   def items, do: Repo.all(Item)
   def get_item(id), do: Repo.get(Item, id)
   def new_item, do: Item.changeset(%Item{})
-  def insert_item(attrs), do: Repo.insert(Item.changeset(%Item{}, attrs))
+  def insert_item(attrs), do: %Item{} |> Item.changeset(attrs) |> Repo.insert()
   def edit_item(id), do: id |> get_item() |> Item.changeset()
-  def update_item(item, updates), do: Repo.update(Item.changeset(item, updates))
+  def update_item(item, updates), do: item |> Item.changeset(updates) |> Repo.update()
 
   # USERS
 
