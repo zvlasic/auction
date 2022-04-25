c3e72db6381b2b1f196651fc31159ec426ddf314
zvlasic <zlayo1@gmail.com>
2022-04-25 14:01:15 +0200

    Add user ctx functions

--- lib/auction.ex
+++ lib/auction.ex
@@ -1,6 +1,8 @@
 defmodule Auction do
   alias Auction.Repo
-  alias Auction.Schemas.Item
+  alias Auction.Schemas.{Item, User}
+
+  # ITEMS
 
   def items, do: Repo.all(Item)
   def get_item(id), do: Repo.get(Item, id)
@@ -8,4 +10,10 @@ defmodule Auction do
   def insert_item(attrs), do: Repo.insert(Item.changeset(%Item{}, attrs))
   def edit_item(id), do: id |> get_item() |> Item.changeset()
   def update_item(item, updates), do: Repo.update(Item.changeset(item, updates))
+
+  # USERS
+
+  def get_user(id), do: Repo.get!(User, id)
+  def new_user, do: User.changeset_with_password(%User{})
+  def insert_user(params), do: %User{} |> User.changeset_with_password(params) |> Repo.insert()
 end
