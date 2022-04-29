9a7d7d708a226743974f9cb8ba63f5b56a467892
zvlasic <zlayo1@gmail.com>
2022-04-29 12:02:45 +0200

    Set session on register

--- lib/auction_web/controllers/user_controller.ex
+++ lib/auction_web/controllers/user_controller.ex
@@ -2,21 +2,24 @@ defmodule AuctionWeb.UserController do
   use AuctionWeb, :controller
 
   def show(conn, %{"id" => id}) do
-    IO.inspect("in SHOW")
     user = Auction.get_user(id)
     render(conn, "show.html", user: user)
   end
 
   def new(conn, _params) do
-    IO.inspect("in new")
     user = Auction.new_user()
     render(conn, "new.html", user: user)
   end
 
   def create(conn, %{"user" => user_params}) do
     case Auction.insert_user(user_params) do
-      {:ok, user} -> redirect(conn, to: Routes.user_path(conn, :show, user))
-      {:error, user} -> render(conn, "new.html", user: user)
+      {:ok, user} ->
+        conn
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.user_path(conn, :show, user))
+
+      {:error, user} ->
+        render(conn, "new.html", user: user)
     end
   end
 end
